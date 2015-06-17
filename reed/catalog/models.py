db4b06c472cf6eb251bbfeb3c23b3ad595e35575
Eugene <yeo.eugene.oey@gmail.com>
2015-06-17 09:15:57 +0800

    Signals user name for author

--- reed/catalog/models.py
+++ reed/catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 from .queryset import BookQuerySet
 
@@ -33,3 +35,12 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def user_for_author(sender, instance, created, **kwargs):
+    if created:
+        user = User.objects.filter(email=instance.email).first()
+        full_name = user.get_full_name()
+        Author.objects.filter(pk=instance.pk).update(name=full_name)
+
+post_save.connect(user_for_author, sender=Author)
